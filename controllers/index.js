4fe95051a3eeb4b4ed45c82379e0828fd7b9b9e3
Antonio Neto <antonionetobhz@gmail.com>
2020-09-10 13:15:07 -0300

    initial commit

--- controllers/index.js
+++
@@ -1,9 +0,0 @@
-const userController = require('./usersController');
-const loginController = require('./loginController');
-const recipesController = require('./recipesController');
-
-module.exports = {
-  userController,
-  loginController,
-  recipesController,
-};
--- services/index.js
+++
@@ -1,9 +0,0 @@
-const usersService = require('./usersService');
-const loginService = require('./loginService');
-const recipesService = require('./recipesService');
-
-module.exports = {
-  usersService,
-  loginService,
-  recipesService,
-};
