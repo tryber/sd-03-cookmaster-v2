7371d5bb1b76aa1d819f3510df5a3191a4fb9ee7
Marco Barbosa <marco.meireles.b@gmail.com>
2020-09-10 20:13:32 -0300

    codeclimate

--- model/model.js
+++ model/model.js
@@ -3,8 +3,8 @@ const mongo = require('./connection');
 
 const createUser = async (name, email, password) => mongo.connect()
   .then((db) => db.collection('users')
-  .insertOne({ name, email, password, role: 'user'}))
-  .then(({ insertedId }) => ({ user: {_id: insertedId, name, email, password, role: 'user'} }))
+  .insertOne({ name, email, password, role: 'user' }))
+  .then(({ insertedId }) => ({ user: { _id: insertedId, name, email, password, role: 'user' } }))
   .catch((error) => error);
 
 const getUserByEmail = async (email) => mongo.connect()
--- services/userServices.js
+++ services/userServices.js
@@ -1,23 +1,33 @@
 const model = require('../model/model');
+
 const createNewUser = async (name, email, password) => {
   // regras de negócio
+  let verify;
+
   if (!name) {
-    return { message: 'Invalid entries. Try again.', status: 400 };
+    verify = { message: 'Invalid entries. Try again.', status: 400 };
   }
   if (!email) {
-    return { message: 'Invalid entries. Try again.', status: 400 };
+    verify = { message: 'Invalid entries. Try again.', status: 400 };
   }
   if (!password) {
-    return { message: 'Invalid entries. Try again.', status: 400 };
+    verify = { message: 'Invalid entries. Try again.', status: 400 };
   }
+
   const emailExistis = await model.getUserByEmail(email);
+
   if (emailExistis && emailExistis.email === email) {
-    return { message: 'Email already registered', status: 409 };
+    verify = { message: 'Email already registered', status: 409 };
   }
-  return await model.createUser(name, email, password)
+
+  if (verify) {
+    return verify;
+  }
+
+  return model.createUser(name, email, password);
 };
 
-const findUserByemail = async (email) => await model.getUserByEmail(email);
+const findUserByemail = async (email) => model.getUserByEmail(email);
 
 module.exports = {
   createNewUser,
