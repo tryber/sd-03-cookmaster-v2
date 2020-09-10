af0992a8218a5fdff0b622a84b98dce6d1033d48
Clayton Pereira <cpereiramt@gmail.com>
2020-09-10 19:16:50 -0400

    Salvando requisito 6 Code climate

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -1,13 +1,10 @@
 const { Router } = require('express');
 const rescue = require('express-rescue');
 
-login = Router();
+const login = Router();
 
 login.post(
   '/',
-  rescue(async (_req, res) => {
-    return res.status(200).json('success');
-  }),
-);
+  rescue(async (_req, res) => (res.status(200).json('success') )),);
 
 module.exports = login;
