31c3bda704862fa0b1ef2e5cbbebcdb5a5d06a02
Hfreitas <hfreitas86@gmail.com>
2020-09-10 21:48:58 -0300

    requisito 6

--- seed.js
+++ seed.js
@@ -1,12 +1,1 @@
-/* Formato obrigatório de documento da collection 'USERS':
-{ "name" : "Erick Jacquin", "email" : "erickjacquin@gmail.com",
-"password" : "12345678", "role" : "user" };
-
-Query criada para atender requisito 6 e em conformidade com o solicitado
-no README e testes */
-db.users.insertOne({
-  name: 'admin',
-  email: 'root@email.com',
-  password: 'admin',
-  role: 'admin',
-});
+"db.users.insertOne({ name: 'admin', email: 'root@email.com', password: 'admin', role: 'admin' });"
