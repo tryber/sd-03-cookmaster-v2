e0f8b4767093e4423346cd40ca5f58f37e138600
Gabryel <gabryel.ryba@gmail.com>
2020-09-11 19:09:28 -0300

    recipes controller hotfix

--- controllers/recipesController.js
+++ controllers/recipesController.js
@@ -7,10 +7,6 @@ const newRecipe = rescue(async (req, res) => {
 
   const result = await recipesService.addRecipe({ name, ingredients, preparation, userId });
 
-  if (result.error && result.error.message === 'Email already registered') {
-    return res.status(409).json(result.error);
-  }
-
   if (result.error) return res.status(400).json(result.error);
 
   return res.status(201).json(result);
