21831e402e87118480a9d851d0df396e853436c4
Gabryel <gabryel.ryba@gmail.com>
2020-09-11 13:12:58 -0300

    removed dotenv variables at Connection.js

--- models/connection.js
+++ models/connection.js
@@ -1,7 +1,7 @@
-require('dotenv').config();
 const { MongoClient } = require('mongodb');
 
-const { MONGO_DB_URL, DB_NAME } = process.env;
+const MONGO_DB_URL = 'mongodb://mongodb:27017/Cookmaster';
+const DB_NAME = 'Cookmaster';
 
 module.exports = () =>
   MongoClient.connect(MONGO_DB_URL, {
