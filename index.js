4571362a8848bd7f94d1455f36d12ea333ba87ff
RobertoAsgon <robertoasgon@gmail.com>
2020-09-11 16:12:22 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 
 const app = express();
-
+// Start
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (request, response) => {
   response.send();
