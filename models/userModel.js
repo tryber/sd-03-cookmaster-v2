91510944752576a4f961e4b3a1e265111f824a83
Cardoso <lsc.cardosoo@gmail.com>
2020-09-10 23:07:19 -0300

    Alterar criaçao de users para try/catch. refatorar userModel.js

--- models/userModel.js
+++ models/userModel.js
@@ -1,16 +1,29 @@
 const { ObjectId } = require('mongodb');
 const connection = require('./connection');
 
-const createUser = async (name, email, password, role) =>
-  connection()
-    .then((db) => db
+// const createUser = async (name, email, password, role) =>
+//   connection()
+//     .then((db) => db
+//       .collection('users')
+//       .insertOne({ name, email, password }))
+//     .then(({ insertedId }) => ({ _id: ObjectId(insertedId), email, password, role }))
+//     .catch((error) => {
+//       console.error(error);
+//       process.exit(1);
+//     });
+
+const createUser = async (name, email, password, role) => {
+  const db = await connection();
+  try {
+    const { insertedId } = await db
       .collection('users')
-      .insertOne({ name, email, password }))
-    .then(({ insertedId }) => ({ _id: ObjectId(insertedId), email, password, role }))
-    .catch((error) => {
-      console.error(error);
-      process.exit(1);
-    });
+      .insertOne({ name, email, password });
+    return { _id: ObjectId(insertedId), email, password, role };
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
+};
 
 const getUserByEmail = async (email) =>
   connection()
