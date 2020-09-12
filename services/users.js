39563dc6d202c0d6eb3920dfa09ad0295bbdc482
brenomesquita <brenomesquitaribeiro@gmail.com>
2020-09-12 01:23:40 -0300

    first exercise

--- services/users.js
+++ services/users.js
@@ -1,8 +1,10 @@
 const users = require('../models/users');
 const { badRequest, conflict } = require('../MyErrors');
 
+const emailRegx = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
 const setNewUser = async (name, email, password) => {
-  if (!name || !email || !password) return badRequest('Invalid entries. Try again.');
+  if (!name || !email || !emailRegx.test(email) || !password) return badRequest('Invalid entries. Try again.');
   const valid = await users.findUserByEmail(email);
   if (valid !== null) return conflict('Email already registered');
   const infos = await users.setNewUser(name, email, password);
