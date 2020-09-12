19878042cb032276153ca7759f81c2bbdcd99de5
Clayton Pereira <cpereiramt@gmail.com>
2020-09-11 20:34:14 -0400

    Salvando requisito 1

--- models/UserModel.js
+++ models/UserModel.js
@@ -1,5 +1,6 @@
 const { connect } = require('./DbConnection');
 
+
 const createUserModel = async ({ name, email, password }) => {
   let result = '';
   let user = '';
@@ -9,11 +10,20 @@ const createUserModel = async ({ name, email, password }) => {
     name,
     email,
     password,
+    role:'user',
   });
   const listUser = result.ops[0];
   user = { user: listUser };
   return user;
 };
+
+const searchByEmail =  async (email) => {
+  const db = await connect();
+  result = await db.collection('user').findOne({email:email});
+  return result.email;
+}
+
 module.exports = {
   createUserModel,
+  searchByEmail,
 };
--- services/UserService.js
+++ services/UserService.js
@@ -1,18 +1,30 @@
 const userModel = require('../models/UserModel');
 
+
+const isValidEmail = (email) => {
+  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+}
+
 const createUserService = async (name, email, password) => {
   const userInfo = { name, email, password };
+ 
   if (name === undefined) {
     return { code: 'invalid_data', message: 'Invalid entries. Try again.' };
   }
-
-  if (email === undefined) {
+   
+  const isEMailValid = isValidEmail(email);
+  if (!isEMailValid) {
+    return { code: 'invalid_data', message: 'Invalid entries. Try again.' };
+  }
+  if (!email) {
     return { code: 'invalid_data', message: 'Invalid entries. Try again.' };
   }
 
   if (password === undefined) {
     return { code: 'invalid_data', message: 'Invalid entries. Try again.' };
   }
+  
   const createUser = await userModel.createUserModel(userInfo);
   return createUser;
 };
