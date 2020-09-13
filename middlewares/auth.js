1514c9cb46bf27fa78354b5e4f5bd65ce0f01ac0
brenomesquita <brenomesquitaribeiro@gmail.com>
2020-09-13 01:11:15 -0300

    third exercise

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,5 +1,5 @@
 const jwt = require('jsonwebtoken');
-const { badRequest, unauthorized } = require('../MyErrors');
+const { unauthorized } = require('../MyErrors');
 const users = require('../models/users');
 
 const authMiddleware = async (req, _res, next) => {
