c2fb0762fe8d9b098aca43a4aaf9f32a87a43b4a
brenomesquita <brenomesquitaribeiro@gmail.com>
2020-09-13 02:03:46 -0300

    third exercise

--- services/recipes.js
+++ services/recipes.js
@@ -1,8 +1,8 @@
 const recipes = require('../models/recipes');
-const { unauthorized } = require('../MyErrors');
+const { badRequest } = require('../MyErrors');
 
 const setNewRecipe = async (name, ingredients, preparation, userId) => {
-  if (!name || !ingredients || !preparation) return unauthorized('Invalid entries. Try again.');
+  if (!name || !ingredients || !preparation) return badRequest('Invalid entries. Try again.');
   const recipeSeted = await recipes.setNewRecipe(name, ingredients, preparation, userId);
   return recipeSeted;
 };
