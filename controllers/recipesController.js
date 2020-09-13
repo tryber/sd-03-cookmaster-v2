1635c418f20b67f28c351b1cac526735b219a6f8
Clayton Pereira <cpereiramt@gmail.com>
2020-09-12 22:54:42 -0400

    Salvando alteraçoes requisito 03

--- controllers/recipesController.js
+++ controllers/recipesController.js
@@ -6,14 +6,15 @@ const { jwtDecodification } = require('./loginController');
 const recipes = Router();
 
 recipes.post('/', rescue(async (req, res) => {
-  const userInfo = await jwtDecodification(req.headers.authorization);
-  const result = await recipesServices.createRecipeService(req.body, userInfo, req.headers.authorization);
-  if (result.code) {
+  const { authorization } = req.headers;
+  const userInfo = await jwtDecodification(authorization);
+  const result = await recipesServices.createRecipeService(req.body, userInfo, authorization);
+  console.log(result);
+  if (result !== undefined) {
     if (result.code === 'invalid_token') return res.status(401).json(result);
-
-    return res.status(400).json(result);
+    if (result.code === 'no_name') return res.status(400).json(result);
+    return res.status(201).json(result);
   }
-  return res.status(201).json(result);
 }));
 
 module.exports = recipes;
--- services/recipesService.js
+++ services/recipesService.js
@@ -1,12 +1,11 @@
 const recipesModel = require('../models/recipesModel');
 const userModel = require('../models/UserModel');
 
-const createRecipeService = async (recipesData, userData, token) => {
+const checkInfo = async (recipesData, userData, token) => {
   const { name, ingredients, preparation } = recipesData;
   if (token.length < 170) {
     return { code: 'invalid_token', message: 'jwt malformed' };
   }
-  const userInfo = await userModel.searchByEmail(userData.data);
   if (name === undefined) {
     return { code: 'no_name', message: 'Invalid entries. Try again.' };
   }
@@ -16,8 +15,18 @@ const createRecipeService = async (recipesData, userData, token) => {
   if (ingredients === undefined) {
     return { code: 'no_name', message: 'Invalid entries. Try again.' };
   }
-  const result = await recipesModel.createRecipeModel(name, ingredients, preparation, userInfo._id);
-  return result;
+};
+
+const createRecipeService = async (recipesData, userData, token) => {
+  const { name, ingredients, preparation } = recipesData;
+  const result = await checkInfo(recipesData, userData, token);
+  if (result !== undefined) {
+    return result;
+  }
+  const userInfo = await userModel.searchByEmail(userData.data);
+  const { _id } = userInfo;
+  const createRecipe = await recipesModel.createRecipeModel(name, ingredients, preparation, _id);
+  return createRecipe;
 };
 
 module.exports = {
