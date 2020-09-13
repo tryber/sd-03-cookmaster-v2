e386aeff1429e2cbb5726ba6439ab9f3ebcbb78d
brenomesquita <brenomesquitaribeiro@gmail.com>
2020-09-13 01:18:19 -0300

    third exercise

--- controllers/recipesController.js
+++ controllers/recipesController.js
@@ -1,9 +1,10 @@
 const recipesService = require('../services/recipes');
 
-const setNewRecipe = async (req, res) => {
+const setNewRecipe = async (req, res, next) => {
   const { name, ingredients, preparation } = req.recipe;
   const { userId } = req;
   const recipe = await recipesService.setNewRecipe(name, ingredients, preparation, userId);
+  if (recipe.error) return next(recipe);
   return res.status(201).json(recipe);
 };
 
--- services/recipes.js
+++ services/recipes.js
@@ -1,6 +1,8 @@
 const recipes = require('../models/recipes');
+const { unauthorized } = require('../MyErrors');
 
 const setNewRecipe = async (name, ingredients, preparation, userId) => {
+  if (!name || !ingredients || !preparation) return unauthorized('Invalid entries. Try again.');
   const recipeSeted = await recipes.setNewRecipe(name, ingredients, preparation, userId);
   return recipeSeted;
 };
