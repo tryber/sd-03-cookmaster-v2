4fb173d8feeabb8a380ff48f0607aaa6578a83db
tales <talestales@gmail.com>
2020-09-14 14:28:12 -0300

    f

--- models/connection.js
+++ models/connection.js
@@ -1,16 +1,16 @@
 require('dotenv/config');
 const MongoClient = require('mongodb').MongoClient;
 
-const MONGO_URL = process.env.MONGO_URL;
+const MONGO_DB_URL = process.env.MONGO_DB_URL;
 const DB_NAME = process.env.DB_NAME;
 
-const connection = () => MongoClient.connect(MONGO_URL, {
+const connection = () => MongoClient.connect(MONGO_DB_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
   .then((conn) => conn.db(DB_NAME))
   .catch((err) => {
-    console.log('XXXXXX', MONGO_URL);
+    console.log('XXXXXX', MONGO_DB_URL);
     console.error(err.message, err.stack);
     process.exit(1);
   });
