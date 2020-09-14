d11732d0dff2439694603fd3077d90a4233addd2
tales <talestales@gmail.com>
2020-09-14 10:58:35 -0300

    cc

--- models/connection.js
+++ models/connection.js
@@ -1,20 +1,18 @@
 require('dotenv/config');
 const MongoClient = require('mongodb').MongoClient;
 
-const MONGO_URL = process.env.MONGO_URL
-const DB_NAME=process.env.DB_NAME;
+const MONGO_URL = process.env.MONGO_URL;
+const DB_NAME = process.env.DB_NAME;
 
-const connection = () => {
-  console.log("PRECISAMOS ACHAR ISSO NO TERMINAL", MONGO_URL)
-  return MongoClient.connect(MONGO_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-    .then((conn) => conn.db(DB_NAME))
-    .catch((err) => {
-      console.error(err.message, err.stack);
-      process.exit(1);
-    });
-}
+const connection = () => MongoClient.connect(MONGO_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+  .then((conn) => conn.db(DB_NAME))
+  .catch((err) => {
+    console.error(err.message, err.stack);
+    process.exit(1);
+  });
+
 
 module.exports = connection;
