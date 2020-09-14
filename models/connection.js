cc67be5b78e500a2dbff267e97f66467aab303d6
tales <talestales@gmail.com>
2020-09-14 14:31:13 -0300

    g

--- models/connection.js
+++ models/connection.js
@@ -1,8 +1,10 @@
 require('dotenv/config');
 const MongoClient = require('mongodb').MongoClient;
 
-const MONGO_DB_URL = process.env.MONGO_DB_URL;
-const DB_NAME = process.env.DB_NAME;
+const {
+  MONGO_DB_URL = 'mongodb://mongodb:27017/Cookmaster',
+  DB_NAME = 'Cookmaster',
+} = process.env;
 
 const connection = () => MongoClient.connect(MONGO_DB_URL, {
   useNewUrlParser: true,
