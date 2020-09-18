ac80c1f1bc5ee626e96028529301266ae2955538
rdgbrezolin <brezolinr@gmail.com>
2020-09-18 13:35:15 -0300

    localyes GO

---
+++ services/recipesService.js
@@ -0,0 +1,40 @@
+const { recipesModel } = require('../models');
+
+const addRecipe = async (data) => {
+  const { name, ingredients, preparation, userId } = data;
+  const createdRecipe = await recipesModel.createRecipe(name, ingredients, preparation, userId);
+  return createdRecipe;
+};
+
+const listAllRecipes = async () => {
+  const recipesList = await recipesModel.getAllRecipes();
+  return recipesList;
+};
+
+const getRecipeById = async (id) => {
+  if (id.length !== 24) return { error: { message: 'recipe not found' } };
+
+  const recipe = await recipesModel.getRecipeById(id);
+
+  if (recipe === null) return { error: { message: 'recipe not found' } };
+
+  return recipe;
+};
+
+const updateRecipe = async (id, userId, name, ingredients, preparation) => {
+  const recipe = await recipesModel.updateRecipe(id, userId, name, ingredients, preparation);
+  return recipe;
+};
+
+const deleteRecipe = async (id) => {
+  await recipesModel.deleteRecipe(id);
+  return null;
+};
+
+module.exports = {
+  addRecipe,
+  listAllRecipes,
+  getRecipeById,
+  updateRecipe,
+  deleteRecipe,
+};
