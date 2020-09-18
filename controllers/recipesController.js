3caa16b9caf85dc62b3d0e1ea97edd4eb2a9da8c
rdgbrezolin <brezolinr@gmail.com>
2020-09-18 11:12:03 -0300

    esqueleto

---
+++ controllers/recipesController.js
@@ -0,0 +1,55 @@
+const rescue = require('express-rescue');
+const { recipesService } = require('../services');
+
+const newRecipe = rescue(async (req, res) => {
+  const { name, ingredients, preparation } = req.body;
+  const { userId } = req.user;
+
+  const result = await recipesService.addRecipe({ name, ingredients, preparation, userId });
+
+  if (result.error) return res.status(400).json(result.error);
+
+  return res.status(201).json(result);
+});
+
+const showAllRecipes = rescue(async (_req, res) => {
+  try {
+    const result = await recipesService.listAllRecipes();
+    return res.status(200).json(result);
+  } catch (error) {
+    res.status(500).json({ error });
+  }
+});
+
+const showRecipeById = rescue(async (req, res) => {
+  const { id } = req.params;
+  const result = await recipesService.getRecipeById(id);
+
+  if (result.error) return res.status(404).json(result.error);
+
+  return res.status(200).json(result);
+});
+
+const editRecipe = rescue(async (req, res) => {
+  const { id } = req.params;
+  const { name, ingredients, preparation } = req.body;
+  const { _id: userId } = req.user;
+
+  const result = await recipesService.updateRecipe(id, userId, name, ingredients, preparation);
+  return res.status(200).json(result);
+});
+
+const deleteRecipe = rescue(async (req, res) => {
+  const { id } = req.params;
+  await recipesService.deleteRecipe(id);
+
+  res.status(204).json();
+});
+
+module.exports = {
+  newRecipe,
+  showAllRecipes,
+  showRecipeById,
+  editRecipe,
+  deleteRecipe,
+};
--- index.js
+++ index.js
@@ -1,8 +1,31 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const { usersController, recipesController } = require('./controllers');
+const { verifyUser, authLogin, verifyRecipe, authToken } = require('./middlewares');
 
 const app = express();
+
+app.use(bodyParser.json());
+
+const PORT = 3000;
+
+app.listen(PORT, () => console.log(`ouvindo na porta ${PORT}`));
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (request, response) => {
   response.send();
 });
+
+app.post('/users', verifyUser, usersController.newUser);
+
+app.post('/login', authLogin);
+
+app.post('/recipes', verifyRecipe, authToken, recipesController.newRecipe);
+
+app.get('/recipes', recipesController.showAllRecipes);
+
+app.get('/recipes/:id', recipesController.showRecipeById);
+
+app.put('/recipes/:id', authToken, recipesController.editRecipe);
+
+app.delete('/recipes/:id', authToken, recipesController.deleteRecipe);
