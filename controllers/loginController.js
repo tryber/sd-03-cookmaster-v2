256305478cd45ec699b034f3eb93cb3dc1b39b06
luldelrey <rpgstrikered@gmail.com>
2020-09-21 14:00:32 -0300

    saving progress

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -1,3 +1,8 @@
 // service
+const login = async (req, res) => {
+  const { email, user } = req.body;
+  
+  res.status(404);
+};
 
-module.exports = async (_req, res) => res.status(404);
+module.exports = { login };
--- services/userServices.js
+++ services/userServices.js
@@ -1,13 +1,20 @@
 // model
+const jwt = require('jsonwebtoken');
 const { createUser, getUserByEmail } = require('../models/userModel');
 
-const validateUser = async (name, email, password) => {
+const SECRET = 'alaalaoluisefera';
+const JWTCONFIG = {
+  expiresIn: '7d',
+  algorithm: 'HS256',
+};
+
+const validateUser = async (name, email, password, type) => {
   const validEmail = /^[^@\s]+@[^@\s]+\.[^@\s]+$/.test(email);
   const duplicate = await getUserByEmail(email);
   switch (true) {
     case (!name || !email || !password || !validEmail):
       return { ok: false, status: 400, message: 'Invalid entries. Try again.' };
-    case (duplicate && duplicate.email === email):
+    case (type === 'REGISTER' && duplicate && duplicate.email === email):
       return { ok: false, status: 409, message: 'Email already registered' };
     default:
       return { ok: true };
@@ -15,7 +22,7 @@ const validateUser = async (name, email, password) => {
 };
 
 const CreateUser = async (name, email, password) => {
-  const validation = await validateUser(name, email, password);
+  const validation = await validateUser(name, email, password, 'REGISTER');
   if (validation.ok) {
     const user = await createUser(name, email, password);
     return { ok: true, user };
@@ -23,6 +30,15 @@ const CreateUser = async (name, email, password) => {
   return validation;
 };
 
+const LogUser = async (uEmail, uPassword) => {
+  // so some token things
+  const { _id, email, role } = await getUserByEmail();
+  const token = jwt.sign({ user }, SECRET, JWTCONFIG)
+  if (email && password) return { token: 'blablabla' };
+  return false;
+};
+
 module.exports = {
   CreateUser,
+  LogUser,
 };
