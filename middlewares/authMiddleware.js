7b4dba6d3f003523c9bc414cb717c4a7d690a147
luldelrey <rpgstrikered@gmail.com>
2020-09-22 00:38:52 -0300

    saving progress

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,22 +1,22 @@
 const jwt = require('jsonwebtoken');
 
-const tokenHandler = (token) => {
-  const SECRET = 'alaalaoluisefera';
-  return jwt.verify(token, SECRET);
-};
-
-const verifyToken = (required) => async (req, _res, next) => {
+const tokenHandler = (token, next) => {
   try {
-    const { authorization } = req.headers;
-    if (!required) return next();
-    if (!authorization) {
-      return next({ status: 401, message: 'invalid token' });
-    }
-    req.user = tokenHandler(authorization);
-    return next();
+    const SECRET = 'alaalaoluisefera';
+    return jwt.verify(token, SECRET);
   } catch (err) {
     return next({ status: 401, message: err.message });
   }
+};
+
+const verifyToken = (required) => async (req, _res, next) => {
+  const { authorization } = req.headers;
+  if (!required) return next();
+  if (!authorization) {
+    return next({ status: 401, message: 'invalid token' });
+  }
+  req.user = tokenHandler(authorization, next);
+  return next();
 };
 
 module.exports = verifyToken;
