e2d37470c9009ee99eaa8eb23af7af93602089c2
Gabriel Oliveira Ferreira <gabril1335555@gmail.com>
2020-10-11 17:50:11 -0300

    cc

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -5,25 +5,20 @@ const { usersModel } = require('../models');
 
 const JWT_SECRET = '1q2w3e4r';
 
-module.exports = (required = true) =>  async (req, _res, next) => {
+module.exports = (required = true) => async (req, _res, next) => {
   const token = req.headers.authorization;
 
-  if (!required) {
-    return next();
-  }
-
-  if (required && !token) {
-    return next(boom.unauthorized('no token informed'));
+  switch (true) {
+    case !required:
+      return next();
+    case required && !token:
+      return next(boom.unauthorized('no token informed'));
   }
 
   try {
     const payload = jwt.verify(token, JWT_SECRET);
 
     const user = await usersModel.userByEmail(payload.email);
-
-    if (!user) {
-      return next(boom.unauthorized('invalid user'));
-    }
 
     req.user = user;
     next();
