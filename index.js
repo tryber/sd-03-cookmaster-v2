3973fd548dfdddf94e2efc80ba6c7310aebf00c3
mirellaalves <mirella.alves@gmail.com>
2020-10-12 18:33:58 -0300

    initial commit - connect db

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
+const bodyParser = require('body-parser');
 const express = require('express');
 
 const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (request, response) => {
   response.send();
 });
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => { console.log(`Lintening on ${PORT}`); });
