0c9021d5d7442a113fe8aee66e72bc5890cca309
mirellaalves <mirella.alves@gmail.com>
2020-10-17 14:41:56 -0300

    include jwt error at authMiddleware for requirement 3

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -7,10 +7,13 @@ module.exports = async (req, res, next) => {
   const token = req.headers.authorization;
   if (!token) return res.status(401).json({ message: 'missing auth token' });
 
-  const payload = jwt.verify(token, JWT_SECRET);
-  const user = await userModel.getUserByEmail(payload.user.email);
-  if (!user) return res.status(401).json({ message: 'user not found' });
-
-  req.user = user;
-  next();
+  try {
+    const payload = jwt.verify(token, JWT_SECRET);
+    const user = await userModel.getUserByEmail(payload.user.email);
+    if (!user) return res.status(401).json({ message: 'user not found' });
+    req.user = user;
+    next();
+  } catch (err) {
+    return res.status(401).json({ message: 'jwt malformed' });
+  }
 };
