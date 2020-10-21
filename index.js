06ef8101c740ad65ae36b24552caeb6f56b58483
wendersonbelko <contato@wendersonbelko.com>
2020-10-21 16:23:03 -0300

    passa no git, pfvr ;-;

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 
 app.use(bodyParser.json());
 
-// não remova esse endpoint, e para o avaliador funcionar
+// não remova esse endpoint, e para o avaliador funcionar.
 app.get('/', (request, response) => {
   response.send();
 });
